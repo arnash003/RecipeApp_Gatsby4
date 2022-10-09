7a6115d9a7fbf18ea6a4fedaa7d98acc77060738
Asher-jay Arenz <asher-jayarenz@Asher-jays-MacBook-Air.local>
2022-10-10 00:41:34 +0900

    Added toggle functionality to the Navbar and made it responsive to all screens

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,29 +1,72 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'gatsby'
-import { FiAlignJustify } from 'react-icons/fi'
+import { FaAlignJustify } from 'react-icons/fa'
+import logo from "../assets/images/logo.svg"
 
 
 const Navbar = () => {
+
+    const [show, setShow] = useState(false);
+
     return (
-        <nav>
-            <FiAlignJustify />
-            <ul>
-                <li>
-                    <Link to="/">Home</Link>
-                </li>
-                <li>
-                    <Link to="/about">About</Link>
-                </li>
-                <li>
-                    <Link to="/contact">Contact</Link>
-                </li>
-                <li>
-                    <Link to="/recipes">Recipes</Link>
-                </li>
-                <li>
-                    <Link to="/tags">Tags</Link>
-                </li>
-            </ul>
+        <nav className='navbar'>
+            <div className='nav-center'>
+                <div className='nav-header'>
+                    <Link to="/">
+                        <img src={logo} alt="simply recipes" to="/" />
+                    </Link>
+                    <button
+                        className='nav-btn'
+                        onClick={() => setShow(!show)}>
+                        <FaAlignJustify />
+                    </button>
+                </div>
+                <div className={show ? "nav-links show-links" : "nav-links"}>
+                    <Link
+                        to='/'
+                        className='nav-link'
+                        activeClassName='active-link'
+                        onClick={() => setShow(false)}
+                    >
+                        Home
+                    </Link>
+                    <Link
+                        to='/recipes'
+                        className='nav-link'
+                        activeClassName='active-link'
+                        onClick={() => setShow(false)}
+                    >
+                        Recipes
+                    </Link>
+                    <Link
+                        to='/tags'
+                        className='nav-link'
+                        activeClassName='active-link'
+                        onClick={() => setShow(false)}
+                    >
+                        Tags
+                    </Link>
+                    <Link
+                        to='/about'
+                        className='nav-link'
+                        activeClassName='active-link'
+                        onClick={() => setShow(false)}
+                    >
+                        About
+                    </Link>
+                    <div
+                        className='nav-link contact-link'
+                    >
+                        <Link
+                            to='contact'
+                            className='btn'
+                            onClick={() => setShow(false)}
+                        >
+                            Contact
+                        </Link>
+                    </div>
+                </div>
+            </div>
         </nav>
     )
 }
