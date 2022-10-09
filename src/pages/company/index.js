dacd38108817759e09839165ec55fb41c5a79f9a
Asher-jay Arenz <asher-jayarenz@Asher-jays-MacBook-Air.local>
2022-10-09 16:19:56 +0900

    first commit

--- src/pages/company/index.js
+++ src/pages/company/index.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
 const index = () => {
-  return (
-    <div>This is the home page</div>
-  )
+    return (
+        <div>hello from company</div>
+    )
 }
 
 export default index
